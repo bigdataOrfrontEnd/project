dbf3b8fd3a402f83f47a9cac36aa222901de96b3
17639638825 <562985990@qq.com>
2024-01-18 15:54:49 +0800

    二级路由配置

--- src/app.js
+++ src/app.js
@@ -9,19 +9,24 @@ import loginV from "@/views/login";
 //创建实例
 const router = new SMERouter("app");
 //添加路由信息
-router.route("/index", (req, res) => {
-  res.render(indexV({ title: "Shirly", message: "锄禾日当午" }));
+router.route("/index", (req, res, next) => {
+  // res.render(indexV({ title: "Shirly", message: "锄禾日当午" }));
+  next(
+    indexV({
+      subRouteContent: res.subRoute(),
+    })
+  );
 });
 
-router.route("/admin", (req, res) => {
+router.route("/index/admin", (req, res) => {
   res.render(adminV());
 });
 
-router.route("/adv", (req, res) => {
-  res.render(advV);
+router.route("/index/adv", (req, res) => {
+  res.render(advV());
 });
 router.route("/login", (req, res) => {
-  res.render(loginV);
+  res.render(loginV());
 });
 
 router.route("*", (req, res) => {
