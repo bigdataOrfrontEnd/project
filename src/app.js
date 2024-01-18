8ad8d1df7902e3e82f6fac7c46cde7576a865140
17639638825 <562985990@qq.com>
2024-01-18 16:41:52 +0800

    组件化抽离

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 //导入路由
 import SMERouter from "sme-router";
+//导入组件
+import Header from "@/components/Header";
+import Sider from "@/components/Sider";
+import Footer from "@/components/Footer";
 //引入模版
 import indexV from "@/views/index";
 import adminV from "@/views/admin";
@@ -13,11 +17,13 @@ router.route("/index", (req, res, next) => {
   // res.render(indexV({ title: "Shirly", message: "锄禾日当午" }));
   next(
     indexV({
+      Header: Header(),
+      Sider: Sider(),
       subRouteContent: res.subRoute(),
+      Footer: Footer(),
     })
   );
 });
-
 router.route("/index/admin", (req, res) => {
   res.render(adminV());
 });
