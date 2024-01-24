f602130c9da90b321cf1141c91b1aa1223cb8ec2
17639638825 <562985990@qq.com>
2024-01-24 09:05:03 +0800

    广告添加成功

--- src/controllers/adv.js
+++ src/controllers/adv.js
@@ -1,16 +1,31 @@
 import advV from "@/views/adv";
+import toastr from "toastr";
 // 导入api函数
 import { postAdv } from "../api/adv";
 //执行广告的添加
-const addAdvExec=async(event)=>{
+const addAdvExec = async (event) => {
   //阻止默认行为
-  event.preventDefault()
+  event.preventDefault();
   //创建FormData对象，以form元素作为参数
- const fd=new FormData(document.addAdvForm)
- //利用纯数组进行判断
- const fdArr=Array.from(fd);
- 
-}
+  const fd = new FormData(document.addAdvForm);
+  //利用纯数组进行判断
+  const fdArr = Array.from(fd);
+  if (!fdArr.every((item) => item[1])) {
+    toastr.error("请将表单填写完整");
+    return;
+  }
+  if (fd.get("advPic").size === 0) {
+    toastr.error("请选择要上传的图片！");
+    return;
+  }
+  fd.forEach((item) => {
+    console.log(item);
+  });
+
+  //使用后端API添加广告
+  const res = await postAdv(fd);
+  console.log(res);
+};
 const prevImgExec = (event) => {
   // 在箭头函数中这样拿到file event.target.files[0]
   const file = event.target.files[0];
@@ -34,5 +49,5 @@ const prevImgExec = (event) => {
 export default (req, res) => {
   res.render(advV());
   document.querySelector("#advPic").addEventListener("change", prevImgExec);
-document.addAdvForm.addEventListener('submit',addAdvExec)
+  document.addAdvForm.addEventListener("submit", addAdvExec);
 };
